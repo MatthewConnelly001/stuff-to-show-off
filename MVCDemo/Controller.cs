0651ea29625a47b61e6a1239b9aac5006e855c8e
MatthewConnelly001 <42534260+MatthewConnelly001@users.noreply.github.com>
2022-01-15 16:06:21 +1100

    Improved coding standard & fully removed whitespaces from input.

--- MVCDemo/Controller.cs
+++ MVCDemo/Controller.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 
 static class Controller
 {
-    private const string takeInput = "take";
-    private const string addInput = "add";
-    private const string showBasketInput = "show";
-    private const string helpInput = "help";
-    private const string exitInput = "exit";
+    private const string TAKE_INPUT = "take";
+    private const string ADD_INPUT = "add";
+    private const string SHOWBASKET_INPUT = "show";
+    private const string HELP_INPUT = "help";
+    private const string EXIT_INPUT = "exit";
 
     private static Dictionary<string, string> inputDescriptions = new Dictionary<string, string>{
-        {$"{takeInput} <item>", "Takes an item from the basket."},
-        {$"{addInput} <item>", "Adds an item to the basket."},
-        {showBasketInput, "Displays all items in the basket."},
-        {helpInput, "Displays information about this app and input commands."},
-        {exitInput, "Closes the application"},
+        {$"{TAKE_INPUT} <item>", "Takes an item from the basket."},
+        {$"{ADD_INPUT} <item>", "Adds an item to the basket."},
+        {SHOWBASKET_INPUT, "Displays all items in the basket."},
+        {HELP_INPUT, "Displays information about this app and input commands."},
+        {EXIT_INPUT, "Closes the application"},
     };
 
 
@@ -28,28 +28,30 @@ static class Controller
         string[] inputTokens = { };
         while(true)
         {
-            inputTokens = View.Input(); 
+            inputTokens = View.Input().ToLower().Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
 
+            // This will never occur due to split function.
             if (inputTokens.Length == 0)
             {
                 View.DisplayNoInput();
             }
 
+            // Selecting command made from first input token.
             switch(inputTokens[0])
             {
-                case helpInput:
+                case HELP_INPUT:
                     View.DisplayHelp(inputDescriptions);
                     break;
-                case takeInput:
+                case TAKE_INPUT:
                     Model.TakeItem( (inputTokens.Length >= 2) ? inputTokens[1] : null );
                     break;
-                case addInput:
+                case ADD_INPUT:
                     Model.AddItem( (inputTokens.Length >= 2) ? inputTokens[1] : null );
                     break;
-                case showBasketInput:
+                case SHOWBASKET_INPUT:
                     View.DisplayShowBasket(Model.basket);
                     break;
-                case exitInput:
+                case EXIT_INPUT:
                     return;
                 default:
                     View.DisplayUnknownInput(inputTokens[0]);
--- MVCDemo/View.cs
+++ MVCDemo/View.cs
@@ -5,10 +5,12 @@ using System.Collections.Generic;
 static class View
 {
     // Getting the input.
-    public static string[] Input(string prompt="")
+    public static string Input(string prompt="")
     {
         Console.Write($"{prompt}>");
-        return Console.ReadLine().Split(' ');
+        String? input = Console.ReadLine(); // to prevent annoying possible null reference return warning.
+        Console.WriteLine();
+        return input == null ? "" : input;
     }
 
     // Invalid input scenarios.
@@ -16,20 +18,17 @@ static class View
     {
         Console.WriteLine($"No input made. Please type a command.");
     }
-    public static void DisplayUnknownInput(string inputToken)
-    {
-        Console.WriteLine($"'{inputToken}' is not defined. Please type a command.");
-    }
+    public static void DisplayUnknownInput(string inputToken) => Console.WriteLine($"'{inputToken}' is not defined. Please type a command.");
 
     // Taking item outcomes.
-    public static void DisplayTakeItemFailed(string name) { Console.WriteLine($"There is no {name} in basket. Could not take any."); }
-    public static void DisplayTakeItemSucceeded(string name) { Console.WriteLine($"Took {name} from basket."); }
-    public static void DisplayTakeItemUnspecified() { Console.WriteLine($"Could not take item from basket. Item name not specified."); }
+    public static void DisplayTakeItemFailed(string name) => Console.WriteLine($"There is no {name} in basket. Could not take any.");
+    public static void DisplayTakeItemSucceeded(string name) => Console.WriteLine($"Took {name} from basket.");
+    public static void DisplayTakeItemUnspecified() => Console.WriteLine($"Could not take item from basket. Item name not specified.");
     
     // Adding item outcomes. (No scenarios of failure exists)
-    public static void DisplayAddItemFailed(string name) { Console.WriteLine($"Cannot add not add {name} to basket."); }
-    public static void DisplayAddItemSucceeded(string name) { Console.WriteLine($"Added {name} to basket."); }
-    public static void DisplayAddItemUnspecified() { Console.WriteLine($"Cannot add item to basket. Item name not specified."); }
+    public static void DisplayAddItemFailed(string name) => Console.WriteLine($"Cannot add not add {name} to basket.");
+    public static void DisplayAddItemSucceeded(string name) => Console.WriteLine($"Added {name} to basket.");
+    public static void DisplayAddItemUnspecified() => Console.WriteLine($"Cannot add item to basket. Item name not specified.");
     
     // Displays all commands in Controller.
     public static void DisplayHelp(Dictionary<string, string> inputDescriptions)
